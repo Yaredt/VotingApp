90b37468fa2cdfd9661f4a3dc72f8ab6f524cc85
Yaredt <yaredt@gmail.com>
2016-10-14 17:11:37 -0400

    Add node app with test

--- VotingApp/server.js
+++ VotingApp/server.js
@@ -1,6 +1,11 @@
-﻿var http = require('http');
-var port = process.env.port || 1337;
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+﻿var express = require('express');
+
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('hello world');
+});
+
+app.listen(process.env.PORT || 5000);
+
+module.exports = app;
--- VotingApp/typings/index.d.ts
+++ VotingApp/typings/index.d.ts
@@ -1,1 +1,4 @@
+/// <reference path="globals/express/index.d.ts" />
+/// <reference path="globals/mocha/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
+/// <reference path="globals/supertest/index.d.ts" />
