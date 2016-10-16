c4815299584403e09a583324b07e08a792004f9e
Yaredt <yaredt@gmail.com>
2016-10-16 03:46:44 -0400

    hellow world

--- VotingApp/test/test.js
+++ VotingApp/test/test.js
@@ -3,6 +3,6 @@ var app = require('../server.js');
 
 describe('GET /', function () {
     it('respond with hello world', function (done) {
-        request(app).get('/').expect('hello world', done);
+        request(app).get('/').expect('hello world2', done);
     });
 });
